c435e442f0b3ebe5903167414ff489f82b2bd107
Jan Collijs <jan.collijs@inuits.eu>
2015-05-15 14:40:31 +0200

    Feature: remove previous package from repo

---
+++ puppet/remove-previous-package.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+
+ITERATION=$(git rev-list --all | wc -l)
+
+if [ -f Modulefile ]
+then
+  MODULE_NAME=$(
+    grep -i ^name Modulefile \
+      | sed -e "s+\"+'+g" \
+      | cut -d "'" -f2 \
+      | cut -d '-' -f2
+  )
+
+  MODULE_VERSION=$(
+    grep -i ^version Modulefile \
+      | sed -e "s+\"+'+g" \
+      | cut -d "'" -f2
+  )
+fi
+
+echo -e "\n\e[1;34m[\e[00m --- \e[00;32mRemove previous package from packagecloud repository \e[00m--- \e[1;34m]\e[00m\n"
+
+package_cloud yank visibilityspots/puppet-development-modules/el/6 puppet-development-module-$MODULE_NAME-$MODULE_VERSION-$ITERATION.x86_64.rpm
+
